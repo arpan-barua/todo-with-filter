46bd0da896dbd5887aa76c11ef57814d50dbaf23
Arpan Barua <arpanctg7@gmail.com>
2022-09-05 13:49:32 -0400

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [inputs, setInputs] = useState({ text: "", completed: false });
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState("ALL");
+
+  const handleChange = (e) => {
+    setInputs({ ...inputs, text: e.target.value });
+  };
+  const handleAdd = () => {
+    setTodos([...todos, inputs]);
+    setInputs({ ...inputs, text: "" });
+  };
+  const handleTodo = (event, i) => {
+    setTodos(
+      todos.map((todo, k) =>
+        i === k ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+  const updateFilter = (filterStatus) => {
+    setFilter(filterStatus);
+  };
+
+  const todosFilter = () => {
+    if (filter === "ALL") {
+      return todos;
+    } else if (filter === "ACTIVE") {
+      return todos.filter((todo) => !todo.completed);
+    } else if (filter === "COMPLETED") {
+      return todos.filter((todo) => todo.completed);
+    }
+    return todos;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" value={inputs.text} onChange={handleChange} />
+      <button onClick={handleAdd}>Add</button>
+      {todosFilter().map((todo, i) => (
+        <div key={i}>
+          <input type="checkbox" onChange={(event) => handleTodo(event, i)} />
+          <span
+            style={{ textDecoration: todo.completed ? "line-through" : "" }}
+          >
+            {todo.text}
+          </span>
+        </div>
+      ))}
+      <button onClick={() => updateFilter("ALL")}>ALL</button>
+      <button onClick={() => updateFilter("ACTIVE")}>ACTIVE</button>
+      <button onClick={() => updateFilter("COMPLETED")}>COMPLETED</button>
     </div>
   );
-}
-
+};
 export default App;
+
